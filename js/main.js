0a5f06917ff66b80278ffcbed9e9ec749213fa93
Sebastian Ospina <sc.ospc@gmail.com>
2022-04-12 16:32:40 -0500

    chnaged the whole design, pending work on API and populating new divs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 console.log('test');
+
+window.addEventListener(
+	'DOMContentLoaded',
+	function (e) {
+		// Original JavaScript code by Chirp Internet: chirpinternet.eu
+		// Please acknowledge use of this code by including this header.
+
+		var slide = document.getElementById('slide');
+		var fadeComplete = function (e) {
+			slide.appendChild(arr[0]);
+		};
+		var arr = slide.getElementsByTagName('img');
+		for (var i = 0; i < arr.length; i++) {
+			arr[i].addEventListener('animationend', fadeComplete, false);
+		}
+	},
+	false
+);
